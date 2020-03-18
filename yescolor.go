321be041c1f9eaa29ea8212f856970b0f7cdad9e
JustinCassart <44966379+JustinCassart@users.noreply.github.com>
2020-03-18 23:01:35 +0100

    Add files via upload

---
+++ yescolor.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	var color string
+	var toDis string
+	args := os.Args[1:]
+	if len(args) > 0 {
+		toDis = strings.Join(args, " ")
+	} else {
+		toDis = "yes"
+	}
+	for {
+		for i := 0; i <= 256; i++ {
+			color = "\x1b[38;5;" + strconv.Itoa(i) + "m"
+			fmt.Print(color + toDis + "\x1b[0m")
+		}
+	}
+}
