8afd20e348048247decd958d9a4e5f13d5d3f2c2
JustinCassart <justin.cassart.4567@gmail.com>
2020-10-26 11:51:45 +0100

    Adding timer

--- yescolor.go
+++ yescolor.go
@@ -2,15 +2,18 @@ package main
 
 import (
 	"fmt"
-	"os"
 	"strconv"
 	"strings"
+	"flag"
+	"time"
 )
 
 func main() {
 	var color string
 	var toDis string
-	args := os.Args[1:]
+	timer := flag.Int("timer", 0, "an int")
+	flag.Parse()
+	args := flag.Args()
 	if len(args) > 0 {
 		toDis = strings.Join(args, " ")
 	} else {
@@ -18,6 +21,7 @@ func main() {
 	}
 	for i := 0; i <= 256; i++ {
 		color = "\x1b[38;5;" + strconv.Itoa(i) + "m"
+		time.Sleep(time.Duration(*timer) * time.Millisecond)
 		fmt.Print(color + toDis + "\x1b[0m")
           if i == 256{
                i = -1
